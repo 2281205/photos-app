9274d4516c1c3bc4cf22c7e25a87a0d3d938b960
Mike Merzliakov <2281205@gmail.com>
2024-07-25 22:07:20 +0300

    add photo description

--- js/main.js
+++ js/main.js
@@ -2,21 +2,18 @@ import { renderPosts } from "./db.js";
 
 function showPost(post) {
   const postItem = document.querySelector("#picture").cloneNode(true).content;
-  console.log(document.querySelector("#picture").cloneNode(true));
-
   const postImg = postItem.querySelector(".picture__img");
   const postComment = postItem.querySelector(".picture__comments");
   const postLikes = postItem.querySelector(".picture__likes");
   postImg.src = post.url;
+  postImg.alt = post.decription;
   postComment.textContent = post.comments.length;
   postLikes.textContent = post.likes;
 
   const appendPost = document.querySelector(".pictures__title");
-  let fragment = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment();
   fragment.appendChild(postItem);
-  appendPost.after(fragment);
+  appendPost.before(fragment);
 }
-
-console.log(renderPosts(25));
 
 renderPosts(10).forEach((element) => showPost(element));
